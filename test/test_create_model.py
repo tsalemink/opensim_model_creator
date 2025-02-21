326768f350243ef83626b0eb494a0527f42d8dc9
tsalemink <tim.nicolas@outlook.com>
2025-02-21 17:52:11 +1300

    Remove redundant documentation in test script
    Hovering mouse over `create_model` function call will display this.

--- test/test_create_model.py
+++ test/test_create_model.py
@@ -12,8 +12,6 @@ def test(input_directory, height, weight):
     marker_data_path = os.path.join(input_directory, "Inputs", "static.pkl")
     static_marker_data = pd.read_pickle(marker_data_path)
 
-    # For creation of muscles, include True statement, for testing purposes include a true statement (sets knee joint optimisation iteration_count to 1, to speed up development)
-    #included a scale_factor for the stl meshes that are brought from lauras code as currently usure if she is scaling or not
     create_model(absolute_path, static_marker_data, height=height, weight=weight, create_muscles=False, testing=True, scale_factor=1000)
 
 
